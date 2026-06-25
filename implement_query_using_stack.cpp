fac11aee29940026b988f8bcc74f945643447aec
Soorjyakanta Sethy <99492724+soorjya@users.noreply.github.com>
2026-06-25 22:49:12 +0530

    Create implement_query_using_stack.cpp

---
+++ implement_query_using_stack.cpp
@@ -0,0 +1,35 @@
+class MyQueue {
+
+private:
+    stack<int> in;
+    stack<int> out;
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        in.push(x);
+    }
+    
+    int pop() {
+        peek();
+        int val = out.top();
+        out.pop();
+        return val;
+    }
+    
+    int peek() {
+        if(out.empty()){
+            while(!in.empty()){
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        return out.top();
+    }
+    
+    bool empty() {
+        return in.empty() && out.empty();
+    }
+};
